3cb5d0441a32171a24342f177567357fb8b9201d
James Nuanez <james@jamesnuanez.com>
2019-12-12 11:08:24 -0800

    Create schema

---
+++ schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS deepgram;
+CREATE DATABASE deepgram;
+USE deepgram;
+
+CREATE TABLE `conferences`
+  (
+    `id`                INT NOT NULL AUTO_INCREMENT,
+    `date_created`      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `meeting_pin`       INT NOT NULL UNIQUE,
+    `description`       VARCHAR(255),
+    PRIMARY KEY (`id`)
+  );
+
+CREATE TABLE `transcriptions`
+  (
+    `id`                INT NOT NULL AUTO_INCREMENT,
+    `time_start`        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `time_end`          TIMESTAMP NULL DEFAULT NULL,
+    `conference_id`     INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`conference_id`) REFERENCES conferences(`id`)
+  );
+
+CREATE TABLE `utterances`
+  (
+    `id`                INT NOT NULL AUTO_INCREMENT,
+    `seq`               INT NOT NULL,
+    `speech`            VARCHAR(65500) NOT NULL,
+    `start`             DECIMAL(12,6),
+    `duration`          DECIMAL(12,6),
+    `confidence`        DECIMAL(5,2),
+    `transcription_id`  INT NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`seq`, `transcription_id`),
+    FOREIGN KEY (`transcription_id`) REFERENCES transcriptions(`id`)
+  );
