0c329907a996754ba3ffd30f261945aa196cff70
James Nuanez <james@jamesnuanez.com>
2019-12-14 12:47:36 -0800

    Update conference: return 409 if PIN is already in use

--- api/conferences/update.js
+++ api/conferences/update.js
@@ -7,14 +7,24 @@ module.exports = async(req, res) => {
       res.status(400).send('Meeting PIN required');
       return;
     }
-    const sqlCheckIfExists = `
+    const sqlCheckIfConfExists = `
       SELECT id
       FROM conferences
       WHERE id = ?
     `;
-    const resultsCheckIfExists = await query(sqlCheckIfExists, req.params.id);
-    if (!resultsCheckIfExists.length) {
+    const resultsCheckIfConfExists = await query(sqlCheckIfConfExists, req.params.id);
+    if (!resultsCheckIfConfExists.length) {
       res.status(404).send('Conference doesn\'t exist');
+      return;
+    }
+    const sqlCheckIfPinInUse = `
+      SELECT id
+      FROM conferences
+      WHERE meeting_pin = ?
+    `;
+    const resultsCheckIfPinInUse = await query(sqlCheckIfPinInUse, req.body['meeting-pin']);
+    if (resultsCheckIfPinInUse.length) {
+      res.status(409).send('A meeting with that PIN already exists');
       return;
     }
     const sqlUpdate = `
