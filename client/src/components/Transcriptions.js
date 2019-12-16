02737f4c7fc866b43ef40973f7e51b16d37141f4
James Nuanez <james@jamesnuanez.com>
2019-12-16 13:18:22 -0800

    Implement routes in React

---
+++ client/src/components/Transcriptions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Transcriptions extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Transcriptions</h1>
+      </div>
+    );
+  }
+}
+
+export default Transcriptions;
---
+++ client/src/components/Utterances.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Utterances extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Utterances</h1>
+      </div>
+    );
+  }
+}
+
+export default Utterances;
